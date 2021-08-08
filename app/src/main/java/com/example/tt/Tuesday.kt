3179410dc4648fd9264c366ba42ebda9ded78ef6
Arihant <arihant.tanwar21@gmail.com>
2021-08-08 22:23:36 +0530

    first commit

---
+++ app/src/main/java/com/example/tt/Tuesday.kt
@@ -0,0 +1,11 @@
+package com.example.tt
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Tuesday : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tuesday)
+    }
+}
